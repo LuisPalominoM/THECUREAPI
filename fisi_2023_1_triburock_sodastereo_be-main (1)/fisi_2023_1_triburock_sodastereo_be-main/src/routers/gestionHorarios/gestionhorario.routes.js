a5aa7caa63b786328d85f19d8d709ebab7f084d1
Palomino Martinez Luis <127364417+LuisPalominoM@users.noreply.github.com>
2023-07-15 13:37:36 -0500

    Delete fisi_2023_1_triburock_sodastereo_be-main (1)/fisi_2023_1_triburock_sodastereo_be-main directory

--- fisi_2023_1_triburock_sodastereo_be-main (1)/fisi_2023_1_triburock_sodastereo_be-main/src/routers/gestionHorarios/gestionhorario.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-
-const gestihorarioRouter = Router();
-
-gestihorarioRouter.get("/consultar-horarios/:idClinica&idEspecialidad&idMedico", () => console.log(""));
-
-export default gestihorarioRouter;
