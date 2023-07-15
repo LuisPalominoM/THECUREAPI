5e4c9859bb6e8cba8bc4c607f6a8a6d908136f0e
Palomino Martinez Luis <127364417+LuisPalominoM@users.noreply.github.com>
2023-07-15 15:45:58 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 
 config();
 export const PORT = process.env.PORT || 3100;
-export const DB_DATABASE = process.env.DB_DATABASE || "clinica";
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "admin";
+export const DB_DATABASE = process.env.DB_DATABASE || "thecureapp";
+export const DB_HOST = process.env.DB_HOST || "dbmysql-thecureapp.mysql.database.azure.com";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "Aa123456";
 export const DB_PORT = process.env.DB_PORT || 3306;
-export const DB_USER = process.env.DB_USER || "root";
+export const DB_USER = process.env.DB_USER || "appadmin";
